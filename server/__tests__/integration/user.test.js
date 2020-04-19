84de8c1453546f5ffae75f2452039f66fea335d0
ederfmatos <ederfmatos@gmail.com>
2020-04-18 23:09:08 -0300

    Teste de email duplicado

--- server/__tests__/integration/user.test.js
+++ server/__tests__/integration/user.test.js
@@ -1,8 +1,13 @@
 import request from 'supertest';
 
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -13,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Eder Matos',
+        email: 'ederfmatos@gmail.com',
+        password_hash: '12345678',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Eder Matos',
+        email: 'ederfmatos@gmail.com',
+        password_hash: '12345678',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
